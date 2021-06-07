87de804a2c5912e7d84582cc24d6266c6299c798
Raeez <raeez.burner@gmail.com>
2021-06-07 12:47:39 +0200

    feat: Add DQL code for Q2 in Practice Quiz 1

--- practice.sql
+++ practice.sql
@@ -70,3 +70,25 @@ FROM (
   ON f.film_id = fa.film_id
 ) sub1;
 -- 5462 rows
+
+
+
+/*
+Write a query that creates a list of actors and movies where the movie length
+was more than 60 minutes. How many rows are there in this query result?
+*/
+WITH t1 AS (
+  SELECT a.first_name || ' ' || a.last_name AS full_name,
+         f.title AS title,
+         f.description AS description,
+         f.length AS length
+  FROM actor a
+  LEFT JOIN film_actor fa
+  ON a.actor_id = fa.actor_id
+  LEFT JOIN film f
+  ON f.film_id = fa.film_id
+)
+SELECT COUNT(t1.*) AS row_count
+FROM t1
+WHERE t1.length > 60;
+-- 4900 rows
