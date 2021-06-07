0a8cdd801e232b0b5388cdfeda790fd78909fd5d
Raeez <raeez.burner@gmail.com>
2021-06-07 12:42:44 +0200

    feat: Add DQL code for Q1 in Practice Quiz 1

---
+++ practice.sql
@@ -0,0 +1,72 @@
+### movies-db Practice SQL
+
+/*
+Let's start with creating a table that provides the following details: actor's
+first and last name combined as full_name, film title, film description and
+length of the movie.
+
+How many rows are there in the table?
+
+*/
+
+-- actor db
+SELECT *
+FROM actor
+LIMIT 10;
+
+SELECT COUNT(*) AS row_count
+FROM actor;
+-- 200 rows
+
+
+SELECT a.first_name || ' ' || a.last_name AS full_name
+FROM actor a;
+
+
+-- film db
+SELECT COUNT(*) AS row_count
+FROM film;
+-- 1000 rows
+
+
+SELECT *
+FROM film
+LIMIT 10;
+
+
+-- film_actor db
+SELECT COUNT(*) AS row_count
+FROM film_actor;
+-- 5462 rows
+
+
+SELECT *
+FROM film_actor
+LIMIT 10;
+
+
+-- Question 1
+SELECT a.first_name || ' ' || a.last_name AS full_name,
+       f.title AS title,
+       f.description AS description,
+       f.length AS length
+FROM actor a
+LEFT JOIN film_actor fa
+ON a.actor_id = fa.actor_id
+LEFT JOIN film f
+ON f.film_id = fa.film_id;
+
+
+SELECT COUNT(sub1.*) AS row_count
+FROM (
+  SELECT a.first_name || ' ' || a.last_name AS full_name,
+         f.title AS title,
+         f.description AS description,
+         f.length AS length
+  FROM actor a
+  LEFT JOIN film_actor fa
+  ON a.actor_id = fa.actor_id
+  LEFT JOIN film f
+  ON f.film_id = fa.film_id
+) sub1;
+-- 5462 rows
