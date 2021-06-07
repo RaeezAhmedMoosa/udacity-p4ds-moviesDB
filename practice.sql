c65ad4d03e4b044345554d363eec51dd2a693bfb
Raeez <raeez.burner@gmail.com>
2021-06-07 13:23:06 +0200

    feat: Add DQL code Q3 in Practice Quiz 1

--- practice.sql
+++ practice.sql
@@ -92,3 +92,58 @@ SELECT COUNT(t1.*) AS row_count
 FROM t1
 WHERE t1.length > 60;
 -- 4900 rows
+
+
+
+/*
+Write a query that captures the actor id, full name of the actor, and counts the
+number of movies each actor has made. (HINT: Think about whether you should
+group by actor id or the full name of the actor.) Identify the actor who has
+made the maximum number movies.
+*/
+
+
+-- actor db
+SELECT a.actor_id AS id,
+       a.first_name || ' ' || a.last_name AS full_name
+FROM actor a;
+
+
+-- JOIN
+SELECT sub1.id AS id,
+       sub1.full_name AS full_name,
+       f.title AS title
+FROM (
+  SELECT a.actor_id AS id,
+         a.first_name || ' ' || a.last_name AS full_name
+  FROM actor a
+) sub1
+LEFT JOIN film_actor fa
+ON sub1.id = fa.actor_id
+LEFT JOIN film f
+ON f.film_id = fa.film_id;
+
+
+-- film count by actor
+WITH t1 AS
+(
+  SELECT sub1.id AS id,
+         sub1.full_name AS full_name,
+         f.title AS title
+  FROM (
+    SELECT a.actor_id AS id,
+           a.first_name || ' ' || a.last_name AS full_name
+    FROM actor a
+  ) sub1
+  LEFT JOIN film_actor fa
+  ON sub1.id = fa.actor_id
+  LEFT JOIN film f
+  ON f.film_id = fa.film_id
+)
+SELECT t1.id,
+       t1.full_name,
+       COUNT(t1.title) AS film_count
+FROM t1
+GROUP BY 1, 2
+ORDER BY 3 DESC;
+-- Gina Degeneres with 42 films
