2ca90120811d013347ae8c35c68386caa1eed2bd
Raeez <raeez.burner@gmail.com>
2021-06-08 13:56:24 +0200

    feat: Add exploratory SQL code for Q2 in Question Set 2

--- question_set_2.sql
+++ question_set_2.sql
@@ -46,7 +46,7 @@ ON s.manager_staff_id = sub1.staff_id
 ORDER BY 1, 2, 3;
 
 
-
+-- Final Working DQL
 SELECT s.store_id AS store_id,
        EXTRACT('year' FROM sub1.year) AS year,
        EXTRACT('month' FROM sub1.month) AS month,
@@ -64,3 +64,80 @@ FROM (
 LEFT JOIN store s
 ON s.manager_staff_id = sub1.staff_id
 ORDER BY 1, 2, 3;
+
+
+
+
+#### Question 2
+
+/*
+We would like to know who were our top 10 paying customers, how many payments
+they made on a monthly basis during 2007, and what was the amount of the monthly
+payments.
+
+Can you write a query to capture the customer name, month and year of payment,
+and total payment amount for each month by these top 10 paying customers?
+*/
+
+-- Exploratory DQLs
+SELECT DATE_TRUNC('month', p.payment_date) AS month,
+       cu.first_name || ' ' || cu.last_name AS full_name,
+       p.amount AS amount
+FROM payment p
+LEFT JOIN customer cu
+ON cu.customer_id = p.customer_id
+ORDER BY 1, 2;
+
+
+
+SELECT DATE_TRUNC('month', p.payment_date) AS month,
+       cu.first_name || ' ' || cu.last_name AS full_name,
+       COUNT(p.payment_id) AS payment_count
+FROM payment p
+LEFT JOIN customer cu
+ON cu.customer_id = p.customer_id
+GROUP BY 1, 2
+ORDER BY 2;
+-- This DQL returns the number of orders per customer for each month
+
+
+SELECT DATE_TRUNC('month', p.payment_date) AS month,
+       cu.first_name || ' ' || cu.last_name AS full_name,
+       SUM(p.amount) AS sum_amount
+FROM payment p
+LEFT JOIN customer cu
+ON cu.customer_id = p.customer_id
+GROUP BY 1, 2
+ORDER BY 1, 2;
+-- This DQL returns the total amount paid per customer for each month
+
+
+-- Working final DQL for Q2 in QS2
+WITH t1 AS (
+  SELECT DATE_TRUNC('month', p.payment_date) AS month,
+         cu.first_name || ' ' || cu.last_name AS full_name,
+         COUNT(p.payment_id) AS payment_count
+  FROM payment p
+  LEFT JOIN customer cu
+  ON cu.customer_id = p.customer_id
+  GROUP BY 1, 2
+),
+     t2 AS
+(
+  SELECT DATE_TRUNC('month', p.payment_date) AS month,
+         cu.first_name || ' ' || cu.last_name AS full_name,
+         SUM(p.amount) AS sum_amount
+  FROM payment p
+  LEFT JOIN customer cu
+  ON cu.customer_id = p.customer_id
+  GROUP BY 1, 2
+)
+SELECT t1.month,
+       t1.full_name,
+       t1.payment_count,
+       t2.sum_amount
+FROM t1
+LEFT JOIN t2
+ON t1.month = t2.month AND t1.full_name = t2.full_name
+ORDER BY 4 DESC
+LIMIT 10;
